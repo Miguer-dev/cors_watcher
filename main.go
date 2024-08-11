175d6b461c433367092eb3de0a3694f096b9d6c8
Miguer-dev <miguerdev@gmail.com>
2024-08-11 12:35:30 -0400

    add application instance and capture interrupt signal

--- main.go
+++ main.go
@@ -1,18 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
+)
 
 const version = "1.0.0"
 
+type application struct {
+	options *options
+	wg      *sync.WaitGroup
+}
+
 func main() {
+	title()
+
 	options := initOptions()
-	optionsValidations := options.validateOptions()
-	if !optionsValidations.valid() {
-		for key, value := range optionsValidations.Errors {
-			fmt.Printf("%s: %s\n", key, value)
-		}
-		return
+
+	app := application{
+		options: options,
+		wg:      &sync.WaitGroup{},
 	}
 
-	fmt.Println("End")
+	optionsValidations := options.validateOptions()
+	if !optionsValidations.valid() {
+		printOptionsErrors(optionsValidations.Errors)
+		os.Exit(1)
+	}
+
+	go func() {
+		quit := make(chan os.Signal, 1)
+		defer close(quit)
+
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+		s := <-quit
+
+		fmt.Println()
+		printWarning(fmt.Sprintf("Signal: %s", s.String()))
+		printWarning("Leaving ...")
+
+		app.wg.Wait()
+
+		os.Exit(0)
+	}()
+
+	time.Sleep(10 * time.Second)
 }
