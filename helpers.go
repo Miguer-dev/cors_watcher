5b693530e8462efd61b1108c76df7e54761e08f9
Miguer-dev <miguerdev@gmail.com>
2024-08-11 18:58:48 -0400

    add readJSON helper

--- helpers.go
+++ helpers.go
@@ -1,6 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"strings"
+)
 
 // execute function on the background with recover on Panic
 func (app *application) backgroundFuncWithRecover(fn func()) {
@@ -17,4 +23,55 @@ func (app *application) backgroundFuncWithRecover(fn func()) {
 
 		fn()
 	}()
+}
+
+// read json and validate json fields
+func readJSON(body io.Reader, dst any) error {
+
+	dec := json.NewDecoder(body)
+
+	dec.DisallowUnknownFields()
+
+	err := dec.Decode(dst)
+	if err != nil {
+		var syntaxError *json.SyntaxError
+		var unmarshalTypeError *json.UnmarshalTypeError
+		var invalidUnmarshalError *json.InvalidUnmarshalError
+
+		switch {
+
+		case errors.As(err, &syntaxError):
+			return errJsonSyntax(syntaxError)
+
+		case errors.Is(err, io.ErrUnexpectedEOF):
+			return errJsonUnexpectedEOF
+
+		case errors.As(err, &unmarshalTypeError):
+			if unmarshalTypeError.Field != "" {
+				return errJsonUnmarshalTypeField(unmarshalTypeError)
+			}
+			return errJsonUnmarshalType(unmarshalTypeError)
+
+		case errors.Is(err, io.EOF):
+			return errJsonEOF
+
+		case strings.HasPrefix(err.Error(), "json: unknown field "):
+			fieldName := strings.TrimPrefix(err.Error(), "json: unknown field ")
+			return errJsonUnknownField(fieldName)
+
+		case errors.As(err, &invalidUnmarshalError):
+			panic(err)
+
+		default:
+			return err
+		}
+	}
+
+	// if the r.body has more info return an error
+	err = dec.Decode(&struct{}{})
+	if err != io.EOF {
+		return errJsonSingleValue
+	}
+
+	return nil
 }
