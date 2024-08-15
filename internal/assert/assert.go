f859656f87ee7864402d5082d8ec3006b7bbbbac
Miguer-dev <miguerdev@gmail.com>
2024-08-14 23:24:42 -0400

    add assert helper for tests

---
+++ internal/assert/assert.go
@@ -0,0 +1,33 @@
+package assert
+
+import (
+	"strings"
+	"testing"
+)
+
+// Equal fail test if "actual" is different to "expected"
+func Equal[T comparable](t *testing.T, actual, expected T) {
+	t.Helper()
+
+	if actual != expected {
+		t.Errorf("got: %v; want: %v", actual, expected)
+	}
+}
+
+// StringContains fail test if "actual" not contain "expectedSubstring"
+func StringContains(t *testing.T, actual, expectedSubstring string) {
+	t.Helper()
+
+	if !strings.Contains(actual, expectedSubstring) {
+		t.Errorf("got: %q; expected to contain: %q", actual, expectedSubstring)
+	}
+}
+
+// NilError fail if actual error is not nil
+func NilError(t *testing.T, actual error) {
+	t.Helper()
+
+	if actual != nil {
+		t.Errorf("got: %v; expected: nil", actual)
+	}
+}
