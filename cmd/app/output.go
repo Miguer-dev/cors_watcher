e03620a672718a32e6ebe557210ce13b0dd06cd1
Miguer-dev <miguerdev@gmail.com>
2024-08-19 16:48:53 -0400

    add print response table

--- cmd/app/output.go
+++ cmd/app/output.go
@@ -13,6 +13,11 @@ var successFormat = color.New(color.FgGreen, color.Bold).PrintfFunc()
 var infoFormat = color.New(color.FgHiWhite, color.Bold).PrintfFunc()
 var subtextFormat = color.New(color.FgHiBlack).PrintlnFunc()
 var highlightFormat = color.New(color.FgCyan, color.Bold).PrintFunc()
+var headerFormat = color.New(color.BgBlue, color.FgBlack, color.Bold).PrintFunc()
+var redBackgroundFormat = color.New(color.BgRed, color.FgBlack, color.Bold).PrintFunc()
+var yellowBackgroundFormat = color.New(color.BgYellow, color.FgBlack, color.Bold).PrintFunc()
+var greenBackgroundFormat = color.New(color.BgGreen, color.FgBlack, color.Bold).PrintFunc()
+var cyanBackgroundFormat = color.New(color.BgCyan, color.FgBlack, color.Bold).PrintFunc()
 
 // print text with error format
 func printError(err string) {
@@ -49,3 +54,37 @@ func printTitle() {
 	subtextFormat("                               by Miguer-dev")
 	fmt.Println()
 }
+
+func (transaction *transaction) printTableHeader() {
+	fmt.Println()
+	headerFormat(" " + transaction.request.URL + " ")
+	fmt.Print(" ")
+	headerFormat(" " + transaction.request.Method + " ")
+	fmt.Println()
+	fmt.Println("+------+------+-------------")
+	fmt.Println("|STATUS| SIZE |   Origin    ")
+	fmt.Println("+------+------+-------------")
+}
+
+func (transaction *transaction) printTableTransaction(url string) string {
+	if url != transaction.request.URL {
+		transaction.printTableHeader()
+		url = transaction.request.URL
+	}
+
+	status := transaction.response.statusCode
+	len := transaction.response.length
+
+	fmt.Printf("| %d%s| %d%s| %s", status, spaces(status, 5), len, spaces(len, 5), transaction.request.Headers["Origin"])
+
+	for _, tag := range transaction.tags {
+		fmt.Print(" ")
+		tag.print(tag.info)
+	}
+
+	fmt.Println()
+	return url
+}
+
+// strconv.FormatBool(transaction.response.ACDetected), transaction.response.ACAO, transaction.response.ACAC}
+//"AC Detected", "ACAO", "ACAC"
